a41dbf2a83582b62988872f6c2c97af61445608b
Duongg-jp <itachi.hp9x@gmail.com>
2024-09-19 19:51:32 +0700

    Commit các thay đổi trên nhánh main trước khi chuyển sang Duong, giao diện home

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// check giao diẹn
+Route::view('/contact', 'dashboard.homeowner.pages.contact')->name('contact');
+
+
+
 require __DIR__.'/auth.php';
