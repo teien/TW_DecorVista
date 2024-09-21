9da2326e514bdf719a2744a724ce19f0d6e6d5b2
TrungAdmin <trungaming3@gmail.com>
2024-09-21 10:31:41 +0700

    push code

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -37,6 +37,7 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+        
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ConsultationController;
 use App\Http\Controllers\DesignerProfileController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,7 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware(['auth', 'role:InteriorDesigner'])->group(function() {
+Route::middleware(['auth', 'role:InteriorDesigner'])->group(function () {
     Route::post('/dashboard/designer/{id}/consultation/accept', [ConsultationController::class, 'accept'])->name('designer.consultations.accept');
     Route::post('/dashboard/designer/{id}/consultation/reject', [ConsultationController::class, 'reject'])->name('designer.consultations.reject');
     Route::post('/dashboard/designer/{id}/consultation/complete', [ConsultationController::class, 'complete'])->name('designer.consultations.complete');
@@ -38,12 +39,7 @@ Route::view('/about', 'dashboard.homeowner.pages.info.about');
 
 
 // check giao diẹn room
-Route::view('/living-room', 'dashboard.homeowner.pages.categories.livingroom');
-Route::view('/bed-room', 'dashboard.homeowner.pages.categories.bedroom');
-Route::view('/kitchen', 'dashboard.homeowner.pages.categories.kitchen');
-Route::view('/bath-room', 'dashboard.homeowner.pages.categories.bathroom');
-Route::view('/office', 'dashboard.homeowner.pages.categories.office');
-Route::view('/outdoor', 'dashboard.homeowner.pages.categories.outdoor');
+Route::get('/products', [ProductController::class, 'index']);
 
 
 // check giao diẹn room
@@ -56,4 +52,4 @@ Route::view('/curtains-blinds', 'dashboard.homeowner.pages.themesstyles.curtains
 
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
